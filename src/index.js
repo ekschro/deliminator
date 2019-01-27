6a24fb83ccdbe24ee08d0958620fe9637dce8746
Ericsson Schroeter <ekschro@protonmail.com>
2019-01-27 18:28:39 -0500

    Linting.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,48 @@
-const { curry } = require('ramda')
+const { curry } = require('ramda');
 
 const delimData = curry(
-    (delimiter, data) => `${data}${delimiter}`
+  (delimiter, data) => `${data}${delimiter}`,
 );
 
 const diceData = curry(
-    (delimiter, data) => data.split(delimiter)
+  (delimiter, data) => data.split(delimiter),
 );
 
-const diceDataAsync = curry((delimiter, data) => {
-    return new Promise((resolve) => {
-        resolve(data.split(delimiter));
-    });
-});
+const diceDataAsync = curry((delimiter, data) => new Promise((resolve) => {
+  resolve(data.split(delimiter));
+}));
 
 const trimData = arr => ({
-    complete: arr.slice(0, arr.length - 1),
-    pending: arr[arr.length - 1]
+  complete: arr.slice(0, arr.length - 1),
+  pending: arr[arr.length - 1],
 });
 
-const trimDataAsync = (arr) => {
-    return new Promise((resolve) => {
-        resolve({
-            complete: arr.slice(0, arr.length - 1),
-            pending: arr[arr.length - 1]
-        });
-    });
-};
+const trimDataAsync = arr => new Promise((resolve) => {
+  resolve({
+    complete: arr.slice(0, arr.length - 1),
+    pending: arr[arr.length - 1],
+  });
+});
 
 const diceAndTrimData = curry(
-    (delimiter, data, chunks) => trimData(diceData(delimiter, `${chunks}${data}`))
+  (delimiter, data, chunks) => trimData(diceData(delimiter, `${chunks}${data}`)),
 );
 
 const diceAndTrimDataAsync = curry(
-    (delimiter, data, chunks) => diceDataAsync(delimiter, `${chunks}${data}`).then(trimDataAsync).catch(console.log)
+  (delimiter, data, chunks) => diceDataAsync(delimiter, `${chunks}${data}`)
+    .then(trimDataAsync).catch(err => err),
 );
 
 const create = delimiter => ({
-    delimit: delimData(delimiter),
-    dice: diceData(delimiter),
-    diceAndTrim: diceAndTrimData(delimiter)
+  delimit: delimData(delimiter),
+  dice: diceData(delimiter),
+  diceAndTrim: diceAndTrimData(delimiter),
 });
 
 const createAsync = delimiter => ({
-    delimit: delimData(delimiter),
-    dice: diceDataAsync(delimiter),
-    diceAndTrim: diceAndTrimDataAsync(delimiter)
+  delimit: delimData(delimiter),
+  dice: diceDataAsync(delimiter),
+  diceAndTrim: diceAndTrimDataAsync(delimiter),
 });
 
 module.exports = { create, createAsync };
