e892339fe18ff8efd7e0ee8ece7597f11e678608
Ericsson Schroeter <ekschro@protonmail.com>
2020-02-04 11:24:17 -0500

    Edits for Node 6...

--- __test__/asyncLib.test.js
+++ __test__/asyncLib.test.js
@@ -1,14 +1,14 @@
-let deliminator = require('../lib/index');
+var deliminator = require('../lib/index');
 
 test('Test delimit() method.  Should return "hello;;"',
   () => {
-    let d = deliminator.createAsync(';;');
+    var d = deliminator.createAsync(';;');
     expect(d.delimit('hello')).toBe('hello;;');
   });
 
 test('Test delimit() method.  Should return "hello;aehkadfhfworld;aehkadfhf!;aehkadfhf"',
   () => {
-    let d = deliminator.createAsync(';aehkadfhf');
+    var d = deliminator.createAsync(';aehkadfhf');
 
     let delimitedData = '';
 
@@ -21,7 +21,7 @@ test('Test delimit() method.  Should return "hello;aehkadfhfworld;aehkadfhf!;aeh
 
 test('Test dice() method.  Should return ["hello", "world", "!", ""]',
   () => {
-    let d = deliminator.createAsync(';;;');
+    var d = deliminator.createAsync(';;;');
 
     let delimitedData = '';
 
@@ -34,10 +34,10 @@ test('Test dice() method.  Should return ["hello", "world", "!", ""]',
 
 test('Test diceAndTrim() method.  Should return {complete: ["hello", "world", "!"], pending: ""}',
   () => {
-    let d = deliminator.createAsync(';;;');
+    var d = deliminator.createAsync(';;;');
 
     let delimitedData = '';
-    let pending = '';
+    var pending = '';
 
     delimitedData += d.delimit('hello');
     delimitedData += d.delimit('world');
@@ -52,10 +52,10 @@ test('Test diceAndTrim() method.  Should return {complete: ["hello", "world", "!
 
 test('Test diceAndTrim() method.  Should return {complete: ["hello", "world", "!"], pending: "incomplete"}',
   () => {
-    let d = deliminator.createAsync(';;;');
+    var d = deliminator.createAsync(';;;');
 
     let delimitedData = '';
-    let pending = 'incomplete';
+    var pending = 'incomplete';
 
     delimitedData += d.delimit(' now completed');
     delimitedData += d.delimit('world');
@@ -70,10 +70,10 @@ test('Test diceAndTrim() method.  Should return {complete: ["hello", "world", "!
 
 test('Test diceAndTrim() method.  Should return {complete: ["hello", "world", "!"], pending: "incomplete data"}',
   () => {
-    let d = deliminator.createAsync('$$$');
+    var d = deliminator.createAsync('$$$');
 
     let delimitedData = '';
-    let pending = 'incomplete';
+    var pending = 'incomplete';
 
     delimitedData += d.delimit(' hello');
     delimitedData += d.delimit('world');
