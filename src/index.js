9240278862a0cbaaaa7f5fa27ab2a732824a0357
Ericsson Schroeter <ekschro@protonmail.com>
2020-02-04 10:45:17 -0500

    Remove the need for Ramda.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
-const { curry } = require('ramda');
+const delimData = delimiter => data => `${data}${delimiter}`;
 
-const delimData = curry((delimiter, data) => `${data}${delimiter}`);
+const diceData = delimiter => data => data.split(delimiter);
 
-const diceData = curry((delimiter, data) => data.split(delimiter));
-
-const diceDataAsync = curry(async (delimiter, data) => data.split(delimiter));
+const diceDataAsync = delimiter => async data => data.split(delimiter);
 
 const trimData = arr => ({ complete: arr.slice(0, arr.length - 1), pending: arr[arr.length - 1] });
 
 const trimDataAsync = async arr => trimData(arr);
 
-const diceAndTrimData = curry(
-  (delimiter, data, chunks) => trimData(diceData(delimiter, `${chunks}${data}`)),
-);
+const diceAndTrimData = delimiter => (data, chunks) => trimData(diceData(delimiter)(`${chunks}${data}`));
 
-const diceAndTrimDataAsync = curry(
-  async (delimiter, data, chunks) => {
-    const diced = await diceDataAsync(delimiter, `${chunks}${data}`);
-    return trimDataAsync(diced);
-  }
-);
+const diceAndTrimDataAsync = delimiter => async (data, chunks) => {
+  const diced = await diceDataAsync(delimiter)(`${chunks}${data}`);
+  return trimDataAsync(diced);
+};
 
 const create = delimiter => ({
   delimit: delimData(delimiter),
